4e04e31e761df081fb87d8a448bb7cd822cee331
Dmitry Kulik <recc1337@gmail.com>
2022-09-24 06:52:35 +0300

    Add id's for login page's elements

---
+++ elements.py
@@ -0,0 +1,14 @@
+BUTTONS = {
+  'NOTIFICATIONS_DENY': "com.android.permissioncontroller:id/permission_deny_button",
+  'LOGIN': "by.com.life.lifego:id/primaryButton",
+}
+
+FIELDS = {
+  'PHONE': "by.com.life.lifego:id/phone",
+  'PASSWORD': "by.com.life.lifego:id/password",
+
+}
+
+CHECKBOXES = {
+  'KEEP_PASSWORD': "by.com.life.lifego:id/checkBoxPassword"
+}
