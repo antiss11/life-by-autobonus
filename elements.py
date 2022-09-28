12f28de878245a2dff5ec126b9b420df0a6f973c
Dmitry Kulik <recc1337@gmail.com>
2022-09-28 16:13:15 +0300

    Code formatting

--- elements.py
+++ elements.py
@@ -1,28 +1,25 @@
 BUTTONS = {
-    'NOTIFICATIONS_DENY_ID': "com.android.permissioncontroller:id/permission_deny_button",
-    'LOGIN_ID': "by.com.life.lifego:id/primaryButton",
-    'HOTBAR_FIRST_ID': 'by.com.life.lifego:id/first_button',
-    'NEXT_ID': 'by.com.life.lifego:id/nextButton',
+    "NOTIFICATIONS_DENY_ID": "com.android.permissioncontroller:id/permission_deny_button",
+    "LOGIN_ID": "by.com.life.lifego:id/primaryButton",
+    "HOTBAR_FIRST_ID": "by.com.life.lifego:id/first_button",
+    "NEXT_ID": "by.com.life.lifego:id/nextButton",
 }
 
 FIELDS = {
-    'PHONE_ID': "by.com.life.lifego:id/phone",
-    'PASSWORD_ID': "by.com.life.lifego:id/password",
-
+    "PHONE_ID": "by.com.life.lifego:id/phone",
+    "PASSWORD_ID": "by.com.life.lifego:id/password",
 }
 
-CHECKBOXES = {
-    'KEEP_PASSWORD_ID': "by.com.life.lifego:id/checkBoxPassword"
-}
+CHECKBOXES = {"KEEP_PASSWORD_ID": "by.com.life.lifego:id/checkBoxPassword"}
 
 RADIOS = {
-    'APP_PROTECTION_METHOD_NONE_XPATH': '//*/android.widget.RadioButton[1]',
+    "APP_PROTECTION_METHOD_NONE_XPATH": "//*/android.widget.RadioButton[1]",
 }
 
 TEXT = {
-    'APP_PROTECTION_METHOD_TITLE_ID': 'by.com.life.lifego:id/main_screen_text',
+    "APP_PROTECTION_METHOD_TITLE_ID": "by.com.life.lifego:id/main_screen_text",
 }
 
 GAME = {
-    'TEXT_ID': 'by.com.life.lifego:id/main_text',
+    "TEXT_ID": "by.com.life.lifego:id/main_text",
 }
