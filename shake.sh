4e6359c42df48c2aba20f410c6415c5b14687b2e
Dmitry Kulik <recc1337@gmail.com>
2022-09-28 09:57:29 +0300

    Add `bash` script perform shake action

---
+++ shake.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+set timeout 10
+set host [lindex $argv 0]
+set port [lindex $argv 1]
+set token [lindex $argv 2]
+
+spawn telnet $host $port
+
+expect "OK"
+send "auth $token\r"
+for {set i 0} {$i <= 11} {incr i} {
+	send "rotate\r"
+	sleep 0.2
+}
